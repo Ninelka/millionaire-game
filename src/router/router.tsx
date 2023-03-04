a200a5fc9b1ee240d77f9b221021ef64f55a7379
Ninel <ninelgruzdeva@gmail.com>
2023-03-04 14:01:15 +0500

    feat: add new pages in router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import { GameLayout, MainLayout } from '../layouts';
+import { ActiveGamePage, GameOverPage, NewGamePage } from '../pages';
 
 export const path = {
   START: '/',
@@ -11,10 +12,26 @@ export const router = createBrowserRouter([
   {
     path: path.START,
     element: <MainLayout />,
+    children: [
+      {
+        path: path.START,
+        element: <NewGamePage />,
+      },
+      {
+        path: path.END,
+        element: <GameOverPage />,
+      },
+    ],
   },
   {
     path: path.GAME,
     element: <GameLayout />,
+    children: [
+      {
+        path: path.GAME,
+        element: <ActiveGamePage />,
+      },
+    ],
   },
   {
     path: '*',
