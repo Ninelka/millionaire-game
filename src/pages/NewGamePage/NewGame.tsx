f9014743e34800eea71615d5a767a36de539079c
Ninel <ninelgruzdeva@gmail.com>
2023-03-04 14:31:54 +0500

    feat: add basic content to NewGame page

--- src/pages/NewGamePage/NewGame.tsx
+++ src/pages/NewGamePage/NewGame.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './NewGame.module.css';
+import { path } from '../../router/router';
+import logo from './../../logo.svg';
+
 const NewGame = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  const startGameHandler = () => {
+    navigate(path.GAME);
+  };
+
+  return (
+    <section className={styles.container}>
+      <img src={logo} className={styles.logo} alt="millionaire game logo" />
+      <h1 className={styles.title}>Who wants to be a millionaire?</h1>
+      <button onClick={startGameHandler}>Start</button>
+    </section>
+  );
 };
 
 export default NewGame;
