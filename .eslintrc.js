10e8e6a05fb7bbeee91bfdc581a2dd7b8c79aa72
Ninel <ninelgruzdeva@gmail.com>
2023-03-04 13:01:27 +0500

    setup: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    "eslint:recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': 2,
+  },
+};
