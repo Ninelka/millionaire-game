63f601ccd78e057c2d87a82d25b6a7aec18d78b5
Ninel <ninelgruzdeva@gmail.com>
2023-03-06 01:20:43 +0500

    feat: create GameOver page

--- src/pages/GameOverPage/GameOver.tsx
+++ src/pages/GameOverPage/GameOver.tsx
@@ -1,5 +1,17 @@
+import logo from '../../logo.svg';
+import styles from './GameOver.module.css';
+import { useGame } from '../../hooks/useGame';
+
 const GameOver = () => {
-  return <></>;
+  const { startGameHandler, currentReward } = useGame();
+
+  return (
+    <section className={styles.container}>
+      <img src={logo} className={styles.logo} alt="millionaire game logo" />
+      <h1 className={styles.title}>{`Your reward is $${currentReward}`}</h1>
+      <button onClick={startGameHandler}>Start New Game</button>
+    </section>
+  );
 };
 
 export default GameOver;
