85e04a24ff1d5b4fc3881be0a28eb828122a66b0
Ninel <ninelgruzdeva@gmail.com>
2023-03-06 00:09:39 +0500

    feat: wrap app by provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
+
+const store = setupStore();
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  );
 }
 
 export default App;
