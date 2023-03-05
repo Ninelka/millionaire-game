91768d844717aa0b497604d31b161c984d0c7125
Ninel <ninelgruzdeva@gmail.com>
2023-03-06 01:19:40 +0500

    feat: create ActiveGame page

--- src/pages/ActiveGamePage/ActiveGame.tsx
+++ src/pages/ActiveGamePage/ActiveGame.tsx
@@ -1,5 +1,42 @@
+import React, { useEffect } from 'react';
+import styles from './ActiveGame.module.css';
+import { Question } from '../../components';
+import { useGame } from '../../hooks/useGame';
+import { useAppSelector } from '../../store/store';
+
 const ActiveGame = () => {
-  return <></>;
+  const { getQuestions } = useGame();
+  const { questions, currentQuestionIndex } = useAppSelector(
+    (store) => store.game
+  );
+
+  const currentQuestion = questions[currentQuestionIndex];
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  useEffect(() => {
+    console.log('currentQuestion: ', questions[currentQuestionIndex]);
+  }, [questions, currentQuestion]);
+
+  return (
+    <>
+      <div className={styles.question}>
+        {!currentQuestion && <p>Questions is loading...</p>}
+        {currentQuestion && (
+          <Question
+            correct_answer={currentQuestion.correct_answer}
+            question={currentQuestion.question}
+            incorrect_answers={currentQuestion.incorrect_answers}
+          />
+        )}
+      </div>
+      <div className={styles.rewards}>
+        <div>Here will be reward pyramid</div>
+      </div>
+    </>
+  );
 };
 
 export default ActiveGame;
